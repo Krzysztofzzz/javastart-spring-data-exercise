e5f146c1780245e057b4ec1ae67fb6b6235f638e
Krzysztof <krzubrycki@gmail.com>
2022-02-09 21:55:48 +0100

    implemented Category entity

--- src/main/java/javastart/spring/entities/Customer.java
+++ src/main/java/javastart/spring/entities/Customer.java
@@ -1,14 +1,20 @@
 package javastart.spring.entities;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "customer")
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String peselNumber;
     private String personIDNumber;
+    @ManyToMany
     private List<Device> devices;
 
 }
