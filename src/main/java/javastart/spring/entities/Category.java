fbeb51433f17a55397857101bf95832146c526d1
Krzysztof <krzubrycki@gmail.com>
2022-02-09 21:48:59 +0100

    Created Classes, implemented Device entity

---
+++ src/main/java/javastart/spring/entities/Category.java
@@ -0,0 +1,12 @@
+package javastart.spring.entities;
+
+import java.util.List;
+
+public class Category {
+
+    private Long id;
+    private String name;
+    private String description;
+    private List<Device> devices;
+
+}
---
+++ src/main/java/javastart/spring/entities/Customer.java
@@ -0,0 +1,14 @@
+package javastart.spring.entities;
+
+import java.util.List;
+
+public class Customer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String peselNumber;
+    private String personIDNumber;
+    private List<Device> devices;
+
+}
